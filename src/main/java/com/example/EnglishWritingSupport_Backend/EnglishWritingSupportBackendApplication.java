bdd0c51aff88f3c58f9f3771ba3307814867a285
Tan <nguyenhuytan2004@gmail.com>
2026-03-25 14:48:40 +0700

    init commit

---
+++ src/main/java/com/example/EnglishWritingSupport_Backend/EnglishWritingSupportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnglishWritingSupport_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishWritingSupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishWritingSupportBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnglishWritingSupport_Backend/EnglishWritingSupportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnglishWritingSupport_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishWritingSupportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
